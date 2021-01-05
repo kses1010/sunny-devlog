fd587eeb3325ffc51d8d96c20a6d7a22d56ae0ee
Sunny <thstkd92@gmail.com>
2021-01-05 21:28:47 +0900

    Gatsby 블로그 초기화

---
+++ content/blog/gatsby/hello-gatsby.md
@@ -0,0 +1,15 @@
+---
+title: 'Hello Gatsby!!'
+date: 2021-01-05
+category: 'gatsby'
+draft: false
+---
+
+안녕하세요 블로그장 써니입니다. 블로그 잘부탁해요!!
+
+```java
+public static void main(String[] args) {
+
+    System.out.println("Hello Gatsby!!")
+}
+```
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Sunny's devlog`,
+  description: `Blog posted about ...`,
+  author: `Sunny`,
+  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `kses1010`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'kses1010/sunny-devlog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
